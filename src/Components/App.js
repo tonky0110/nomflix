eb85afc22d253ad8cf91c2524b244249025ce58e
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-10 23:40:27 +0900

    3.3 styled-reset을 이용한 css초기화, GlobalStyles를 이용한 전체 적용

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GlobalStyles from './GlobalStyles';
 import Router from './Router';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
 		return (
 			<>
 				<Router />
+				<GlobalStyles />
 			</>
 		);
 	}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,23 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Header = styled.div``;
-
-const List = styled.ul`
+const Header = styled.div`
+	color: white;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 50px;
 	display: flex;
-	&:hover {
-		background-color: blue;
-	}
+	align-items: center;
+	padding: 0px 10px;
+	background-color: rgba(20, 20, 20, 0.8);
+	box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
-const Item = styled.li``;
 
-const SLink = styled(Link)``;
+const List = styled.ul`display: flex;`;
+
+const Item = styled.li`
+	width: 50px;
+	height: 50px;
+	text-align: center;
+`;
+
+const SLink = styled(Link)`
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 export default () => (
 	<Header>
 		<List>
 			<Item>
-				<SLink to="/">Home</SLink>
+				<SLink to="/">Movies</SLink>
 			</Item>
 			<Item>
 				<SLink to="/tv">Tv</SLink>
