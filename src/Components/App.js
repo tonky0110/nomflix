0025d494c78f9bd70b0b68266c714a1388b12af8
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-10 22:44:32 +0900

    2.1 Router>Route

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Router from './Router';
 
 class App extends Component {
 	render() {
-		return <div>App</div>;
+		return (
+			<>
+				<Router />
+			</>
+		);
 	}
 }
 
---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePresenter from './HomePresenter';
+
+export default class HomeContainer extends React.Component {
+	render() {
+		return <HomePresenter />;
+	}
+}
