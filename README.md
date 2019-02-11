deee811d3e7190ac94b0714c5d98c8b15c305405
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-11 22:54:09 +0900

    5.0 Home Container state구성 및 render에 전달달

--- README.md
+++ README.md
@@ -9,8 +9,14 @@ Learning React and ES6 by building a Movie Discovery App.
 
 ## API Verbs
 
- - [ ] Now playing (Movie)
+- [ ] Now playing (Movie)
 - [ ] Upcoming (Movie)
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
+- [ ] Top Rated (TV)
+- [ ] Popular (TV, X)
 - [ ] Airing Today (TV)
+- [ ] TV Show Detail
+- [ ] Movie Detail
+- [ ] Search (Movie, TV)
+
+
+append_to_response: 추가적인 params을 덧붙여서 추가 결과를 return받기 위한 옵션
--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -2,7 +2,27 @@ import React from 'react';
 import HomePresenter from './HomePresenter';
 
 export default class HomeContainer extends React.Component {
+	// constructor
+	state = {
+		nowPlaying: null,
+		upcoming: null,
+		popular: null,
+		error: null,
+		loading: false
+	};
+	// logic
+
+	// render
 	render() {
-		return <HomePresenter />;
+		const { nowPlaying, upcoming, popular, error, loading } = this.state;
+		return (
+			<HomePresenter
+				nowPlaying={nowPlaying}
+				upcoming={upcoming}
+				popular={popular}
+				error={error}
+				loading={loading}
+			/>
+		);
 	}
 }
