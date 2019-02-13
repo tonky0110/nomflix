2dd5c036ee4b18cb1801fd7f6ffbcf6d71c3db50
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-13 23:39:56 +0900

    5.5 Detail(Route, id타입체크(isNaN))

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -7,7 +7,17 @@ export default class DetailContainer extends React.Component {
 		error: null,
 		loading: false
 	};
+	componentDidMount = async () => {
+		const { match: { params: { id } }, history: { push } } = this.props;
+		const parsedId = parseInt(id);
+
+		if (isNaN(parsedId)) {
+			console.log('parsedId is not Number.');
+			return push('/');
+		}
+	};
 	render() {
+		console.log('Detail - this.props: ', this.props);
 		const { result, error, loading } = -this.state;
 		return <DetailPresenter result={result} error={error} loading={loading} />;
 	}
