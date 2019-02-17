23dafc25099a7be4167e43e908b676e5f8171d3b
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-17 21:51:06 +0900

    Detail Container

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import { moviesApi, tvApi } from 'api';
 import DetailPresenter from './DetailPresenter';
 
 export default class DetailContainer extends React.Component {
-	state = {
-		result: null,
-		error: null,
-		loading: false
-	};
+	// isMovie
+	// this.state.isMovie
+	// 의미의 차이 점. - this.isMovie: class에서 한번도 변경하지 않으면, 내부 변수로 남겨둘 수 있다.
+	constructor(props) {
+		super(props);
+		const { location: { pathname } } = props;
+		this.state = {
+			result: null,
+			error: null,
+			loading: false,
+			isMovie: pathname.includes('/movie/')
+		};
+	}
 	componentDidMount = async () => {
 		const { match: { params: { id } }, history: { push } } = this.props;
+		const { isMovie } = this.state;
 		const parsedId = parseInt(id);
-
 		if (isNaN(parsedId)) {
-			console.log('parsedId is not Number.');
 			return push('/');
 		}
+		try{
+			const { data: result } = isMovie ? await moviesApi.movieDetail(parsedId) : await tvApi.showDetail(parsedId);
+			console.log(result);
+		}catch{
+			this.setState({ error: "Cant't find anything."});
+		}finally{
+			this.setState({ loading: false});
+		}
 	};
+
 	render() {
-		console.log('Detail - this.props: ', this.props);
 		const { result, error, loading } = -this.state;
+		console.log(this.state);
 		return <DetailPresenter result={result} error={error} loading={loading} />;
 	}
 }
