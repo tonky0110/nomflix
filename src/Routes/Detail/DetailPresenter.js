a0516cf5d4f4ceae13c973520e2cf3071542e688
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-23 18:43:51 +0900

    6.9 Detail Presenter ItemContainer && Overview

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -41,6 +41,26 @@ const Cover = styled.div`
 	border-radius: 5px;
 `;
 
+const Data = styled.div`
+	width: 70%;
+	margin-left: 10px;
+`;
+
+const Title = styled.h3`font-size: 32px;`;
+
+const ItemContainer = styled.div`margin: 20px 0;`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`margin: 0 10px;`;
+
+const Overview = styled.p`
+	font-size: 12px;
+	opacity: 0.7;
+	line-height: 2;
+	width: 50%;
+`;
+
 const DetailPresenter = ({ result, error, loading }) => {
 	console.log(result, error, loading);
 	return loading ? (
@@ -58,6 +78,29 @@ const DetailPresenter = ({ result, error, loading }) => {
 						)
 					}
 				/>
+				<Data>
+					<Title>{result.original_title ? result.original_title : result.original_name}</Title>
+					<ItemContainer>
+						<Item>
+							{result.release_date ? (
+								result.release_date.substring(0, 4)
+							) : (
+								result.first_air_date.substring(0, 4)
+							)}
+						</Item>
+						<Divider>·</Divider>
+						<Item>{result.runtime ? result.runtime : result.episode_run_time[0]}</Item>
+						<Divider>·</Divider>
+						<Item>
+							{result.genres &&
+								result.genres.map(
+									(genre, index) =>
+										index === result.genres.length - 1 ? genre.name : `${genre.name} / `
+								)}
+						</Item>
+					</ItemContainer>
+					<Overview>{result.overview && result.overview}</Overview>
+				</Data>
 			</Content>
 		</Container>
 	);
